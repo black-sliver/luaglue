dbfcacbb4df007a0f2ffa0ccef66e0722ae39744
black-sliver <59490463+black-sliver@users.noreply.github.com>
2023-09-02 00:35:43 +0200

    add luaL_testudata compat for lua 5.1

--- luacompat.h
+++ luacompat.h
@@ -32,6 +32,21 @@ static lua_Integer luaL_len(lua_State *L, int idx)
 {
     return (lua_Integer)lua_objlen(L, idx);
 }
+
+LUALIB_API void *luaL_testudata (lua_State *L, int ud, const char *tname) {
+  void *p = lua_touserdata(L, ud);
+  if (p != NULL) {  /* value is a userdata? */
+    if (lua_getmetatable(L, ud)) {  /* does it have a metatable? */
+      lua_getfield(L, LUA_REGISTRYINDEX, tname);  /* get correct metatable */
+      if (lua_rawequal(L, -1, -2)) {  /* does it have the correct mt? */
+        lua_pop(L, 2);  /* remove both metatables */
+        return p;
+      }
+      lua_pop(L, 2);  /* remove both metatables */
+    }
+  }
+  return NULL;  /* to avoid warnings */
+}
 #endif
 
 #ifndef LUA_OK
