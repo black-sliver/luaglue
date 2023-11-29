b3d35b81cdb96e1df58f8c4ed7c28401487fc4db
black-sliver <59490463+black-sliver@users.noreply.github.com>
2023-11-29 22:46:19 +0100

    add missing new file

---
+++ lua_include.h
@@ -0,0 +1,15 @@
+#ifndef LUA_INCLUDE_H
+#define LUA_INCLUDE_H
+
+#ifndef LUA_CPP
+extern "C" {
+#endif
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+#ifndef LUA_CPP
+}
+#endif
+
+#endif /* LUA_INCLUDE_H */
+
