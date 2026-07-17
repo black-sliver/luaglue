77acbd06beb4ae39824a6efd2ef30061be8d39a7
black-sliver <59490463+black-sliver@users.noreply.github.com>
2026-07-17 17:57:32 +0200

    luaref: fix typo in comment

--- luaref.h
+++ luaref.h
@@ -3,7 +3,7 @@
 
 #include "lua_include.h"
 
-/* helper to differenciate between int and ref */
+/* helper to differentiate between int and ref */
 struct LuaRef { int ref=LUA_NOREF; bool valid() const { return ref!=LUA_NOREF; }};
 
 #endif // _LUAGLUE_LUAREF_H
